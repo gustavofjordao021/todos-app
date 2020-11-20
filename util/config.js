0859ff19f61cdbca63da8b7ae659d3e18209217a
gustavofjordao021 <gustavo.jordao021@gmail.com>
2020-11-20 15:16:19 -0500

    Git setup and repo cleanup

---
+++ util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyA7T_-X1S8rQ-lchYmcljFdnQGuEXh2Zow",
+  authDomain: "todos-app-aecb8.firebaseapp.com",
+  databaseURL: "https://todos-app-aecb8.firebaseio.com",
+  projectId: "todos-app-aecb8",
+  storageBucket: "todos-app-aecb8.appspot.com",
+  messagingSenderId: "847897934018",
+  appId: "1:847897934018:web:c16913971d02f70212635b",
+  measurementId: "G-5ZGLD6DQ9X",
+};
