367ad6c0e3257119a3dd1dc0091f61f234144cd0
gustavofjordao021 <gustavo.jordao021@gmail.com>
2020-11-24 22:17:03 -0500

    Avatar upload and routeGuard setup

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
+
 const app = require("express")();
 const functions = require("firebase-functions");
 
-//Todos routes
+//API routes
 app.use("/", require("./routes/todos"));
+app.use("/auth", require("./routes/auth"));
 
 exports.api = functions.https.onRequest(app);
---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: process.env.GOOGLE_API_KEY,
+  authDomain: process.env.GOOGLE_AUTH_DOMAIN,
+  databaseURL: process.env.GOOGLE_DATABASE_URL,
+  projectId: process.env.GOOGLE_PROJECT_ID,
+  storageBucket: process.env.GOOGLE_STORAGE_BUCKET,
+  messagingSenderId: process.env.GOOGLE_MESSAGING_SENDER_ID,
+  appId: process.env.GOOGLE_APP_ID,
+  measurementId: process.env.GOOGLE_MEASUREMENT_ID,
+};
