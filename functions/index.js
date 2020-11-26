db946de0b0f8723506a5dcdd996acc8e0ca95b29
gustavofjordao021 <gustavo.jordao021@gmail.com>
2020-11-26 12:44:02 -0500

    Including dependencies for deploying to Firebase

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,23 @@
 require("dotenv").config();
 
 const app = require("express")();
+const cors = require("cors");
+const helmet = require("helmet");
 const functions = require("firebase-functions");
+
+// CORS setup
+app.use(
+  cors({
+    preflightContinue: true,
+    optionsSuccessStatus: 200,
+    origin: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "HEAD", "PATCH", "DELETE"],
+  })
+);
+
+// Middleware Setup
+app.use(helmet());
 
 //API routes
 app.use("/", require("./routes/todos"));
