f6700e5271c6b96ad6b91abc000429657da07508
gustavofjordao021 <gustavo.jordao021@gmail.com>
2020-12-24 16:23:29 -0500

    Removing avatar routes

--- functions/routes/auth.js
+++ functions/routes/auth.js
@@ -2,7 +2,6 @@
 const fs = require("fs");
 const os = require("os");
 const path = require("path");
-const BusBoy = require("busboy");
 const config = require("../util/config");
 const express = require("express");
 const firebase = require("firebase");
@@ -96,61 +95,6 @@ router.post("/signup", (req, res) => {
           .json({ data: "Something went wrong, please try again" });
       }
     });
-});
-
-//GET Upload profile picture
-router.post("/signup/image", routeGuard, (req, res) => {
-  const busboy = new BusBoy({ headers: req.headers });
-  let imageFileName;
-  let imageToBeUploaded = {};
-
-  busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
-    if (mimetype !== "image/png" && mimetype !== "image/jpeg") {
-      return res.status(400).json({ error: "Wrong file type submited" });
-    }
-    const imageExtension = filename.split(".")[filename.split(".").length - 1];
-    imageFileName = `${req.user.username}.${imageExtension}`;
-    const filePath = path.join(os.tmpdir(), imageFileName);
-    imageToBeUploaded = { filePath, mimetype };
-    file.pipe(fs.createWriteStream(filePath));
-  });
-  const bucket = admin.storage().bucket();
-  bucket
-    .file(`${imageFileName}`)
-    .delete()
-    .then(() => {
-      return;
-    })
-    .catch((error) => {
-      return;
-    });
-  busboy.on("finish", () => {
-    admin
-      .storage()
-      .bucket()
-      .upload(imageToBeUploaded.filePath, {
-        resumable: false,
-        metadata: {
-          metadata: {
-            contentType: imageToBeUploaded.mimetype,
-          },
-        },
-      })
-      .then(() => {
-        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
-        return db.doc(`/users/${req.user.username}`).update({
-          imageUrl,
-        });
-      })
-      .then(() => {
-        return res.status(200).json({ message: "Image uploaded successfully" });
-      })
-      .catch((error) => {
-        console.error(error);
-        return res.status(500).json({ error: error.code });
-      });
-  });
-  busboy.end(req.rawBody);
 });
 
 //GET Get all user details
